a0b4d9b6a83fd0ce379ce0abc28b110a6044d05c
ECostin84 <evghenii.costin@sixt.com>
2023-08-07 14:43:41 +0200

    models was created

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# What categories are the courses
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+# course description
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    prise = models.FloatField()
+    students_qty = models.IntegerField()
+    reviews_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
