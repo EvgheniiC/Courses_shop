039c2171c2b699b63c922bfeefa3b03b66154a11
ECostin84 <evghenii.costin@sixt.com>
2023-08-22 06:50:00 +0200

    api urls ware added

--- api/models.py
+++ api/models.py
@@ -5,12 +5,12 @@ from shop.models import Category, Course
 class CategoryResourse(ModelResource):
     class Meta:
         queryset = Category.objects.all()
-        resourse_name = 'categories'
+        resource_name = 'categories'
         allowed_methods = ['get']
 
 
 class CourseResourse(ModelResource):
     class Meta:
         queryset = Course.objects.all()
-        resourse_name = 'courses'
+        resource_name = 'courses'
         allowed_methods = ['get', 'post', 'delete']
--- base/urls.py
+++ base/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.models import CourseResourse, CategoryResourse
+
+course_resourse = CourseResourse()
+category_resourse = CategoryResourse()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    path('api/', include(course_resourse.urls)),
+    path('api/', include(category_resourse.urls)),
 ]
