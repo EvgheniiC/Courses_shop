6d1a8b2bd5941257cd52c5bb7099f80d68fe6223
ECostin84 <evghenii.costin@sixt.com>
2023-08-22 06:23:59 +0200

    Resourses were added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
 
-# Create your models here.
+
+class CategoryResourse(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resourse_name = 'categories'
+        allowed_methods = ['get']
+
+
+class CourseResourse(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resourse_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
