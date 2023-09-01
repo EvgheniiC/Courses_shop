6f32720e190d69a7f58bf54ce853e08b20d6d880
ECostin84 <evghenii.costin@sixt.com>
2023-09-01 13:01:23 +0200

    admin seite was changed(course + category)

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,35 @@
 from django.contrib import admin
-from . import models
+from .models import Course, Category
 
-admin.site.register(models.Course)
-admin.site.register(models.Category)
+admin.site.site_header = "Courses admin"
+admin.site.site_title = "My Courses"
+admin.site.index_title = "Welcome to the courses admin area"
+
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'prise', 'category')
+
+
+# add table with course in Category seite
+class CourserInline(admin.TabularInline):
+    model = Course
+    exclude = ['created_at']  # without 'created_at' columm
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Dates', {
+            'fields': ['created_at'],
+            'classes': ['collapce']
+        }
+        )
+    ]
+    # add table with course in Category seite
+    inlines = [CourserInline]
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Category, CategoryAdmin)
